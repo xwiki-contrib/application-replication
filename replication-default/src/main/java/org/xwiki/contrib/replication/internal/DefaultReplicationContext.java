9e2fde4084403d115ff3fb834dbfd9a4c620b057
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-07-01 16:28:45 +0200

    REPLICAT-1: API and implementation to transmit data between instances * add missing class in previous commit

---
+++ replication-default/src/main/java/org/xwiki/contrib/replication/internal/DefaultReplicationContext.java
@@ -0,0 +1,76 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.replication.internal;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.context.Execution;
+import org.xwiki.context.ExecutionContext;
+import org.xwiki.contrib.replication.ReplicationContext;
+
+/**
+ * @version $Id$
+ */
+@Component
+@Singleton
+public class DefaultReplicationContext implements ReplicationContext
+{
+    private static final String REPLICATION_MESSAGE = "replication.message";
+
+    @Inject
+    private Execution execution;
+
+    /**
+     * @return true if the execution results from a received replication message, false otherwise
+     */
+    @Override
+    public boolean isReplicationMessage()
+    {
+        ExecutionContext context = this.execution.getContext();
+
+        if (context != null) {
+            Boolean value = (Boolean) context.getProperty(REPLICATION_MESSAGE);
+
+            if (value == Boolean.TRUE) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param replicationMessage true if the execution results from a received replication message, false otherwise
+     */
+    public void setReplicationMessage(boolean replicationMessage)
+    {
+        ExecutionContext context = this.execution.getContext();
+
+        if (context != null && isReplicationMessage() != replicationMessage) {
+            if (replicationMessage) {
+                context.newProperty(REPLICATION_MESSAGE).inherited().initial(replicationMessage).makeFinal().declare();
+            } else {
+                context.removeProperty(REPLICATION_MESSAGE);
+            }
+        }
+    }
+}
